26fb6308d2f3e74bb2d2faff3a27d192697ee3be
Андрей <anastasiiapegar@gmail.com>
2022-10-31 13:54:57 +0300

    init1

---
+++ js/about.6edcf182.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkapex_pages"]=self["webpackChunkapex_pages"]||[]).push([[443],{5399:function(e,n,s){s.r(n),s.d(n,{default:function(){return i}});var t=function(){var e=this;e._self._c;return e._m(0)},u=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=s(1001),l={},r=(0,a.Z)(l,t,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.6edcf182.js.map
